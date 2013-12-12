74978cd03a4df2d96edec158b10f306463688f3e
xd-fjx <xd-fjx@qq.com>
2013-12-12 22:31:15 +0800

    commit

---
+++ xd_oa_v1.0/src/com/fjx/oa/security/init/PermissionConfig.java
@@ -0,0 +1,32 @@
+package com.fjx.oa.security.init;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fjx.common.utils.Dom4jHelper;
+import com.fjx.oa.security.modules.AuthenUrl;
+
+public class PermissionConfig {
+
+	public static final int CREATE = 0;
+	public static final int READ = 1;
+	public static final int UPDATE = 2;
+	public static final int DELETE = 3;
+	
+	/**
+	 * 需要权限控制的URL
+	 */
+	public  Map<String, AuthenUrl> security = new HashMap<String, AuthenUrl>();
+	
+	static{
+		
+		
+		
+	}
+	
+	public void initSecurity() throws Exception{
+		Dom4jHelper.parse("", "UTF-8");
+		
+		
+	}
+}
